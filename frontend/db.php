b37f22dcac579f258d941ecb79007a003fedf55e
Marc <151544777+MarcVillaS@users.noreply.github.com>
2025-06-10 11:11:17 +0200

    Delete frontend directory

--- frontend/db.php
+++
@@ -1,3 +0,0 @@
-<?php
-$pdo = new PDO("mysql:host=localhost;dbname=tasklance;charset=utf8mb4", "root", "");
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
--- frontend/principal/footer.php
+++
@@ -1,5 +0,0 @@
-<footer style="margin-top:40px; background:#222; color:#fff; padding:18px 0; text-align:center; font-size:1em;">
-    &copy; <?= date("Y") ?> TaskLance. Todos los derechos reservados.
-</footer>
-</body>
-</html>
